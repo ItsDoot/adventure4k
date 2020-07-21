caa88df9a1cf5237171ce20ab1848bc96a713071
Christian Hughes <xdotdash@gmail.com>
2020-07-21 14:47:46 -0500

    use `Component#append` instead of `ComponentBuilder#append`

--- src/main/kotlin/pw/dotdash/adventure4k/ext/Component.kt
+++ src/main/kotlin/pw/dotdash/adventure4k/ext/Component.kt
@@ -1,7 +1,14 @@
 package pw.dotdash.adventure4k.ext
 
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.ComponentBuilder
+import net.kyori.adventure.text.ComponentLike
 
-operator fun Component.plus(other: Component): TextComponent =
-    TextComponent.builder().append(this).append(other).build()
+operator fun Component.plus(component: Component): Component =
+    this.append(component)
+
+operator fun Component.plus(component: ComponentLike): Component =
+    this.append(component)
+
+operator fun Component.plus(builder: ComponentBuilder<*, *>): Component =
+    this.append(builder)
